47f2fd7630058d696b79a1d95b091ddb2c01196d
Jarle302 <jarlehtollaksen2@live.no>
2024-05-06 14:16:26 +0200

    fix

--- app/ui/components/VenueList/index.tsx
+++ app/ui/components/VenueList/index.tsx
@@ -3,10 +3,11 @@ import { Venue, Booking } from "../../constants/types";
 
 type bookings = { bookings: Booking[] };
 export const VenueList = async () => {
-  const response = await fetch(
-    "http://localhost:3000/auth?endpoint=holidaze/bookings",
-    { method: "GET", credentials: "include" }
-  );
+  const url = process.env.siteUrl;
+  const response = await fetch(url as string, {
+    method: "GET",
+    credentials: "include",
+  });
   const data = await response.json();
   console.log("venueListDATA:", data);
   const cards = data.data.map((venue: Venue & bookings) => (
