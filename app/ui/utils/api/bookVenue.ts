d906125e133c38e2c5b5a99dd451bc703dbd7041
Jarle302 <jarlehtollaksen2@live.no>
2024-05-19 18:47:32 +0200

    use env for baseurl

--- app/ui/utils/api/bookVenue.ts
+++ app/ui/utils/api/bookVenue.ts
@@ -1,10 +1,13 @@
 "use server";
 
-import { proxyURL } from "../../constants/constants";
 import formDataToObject from "../formDataToObject";
+
+const base = process.env.NEXT_PUBLIC_BASE_URL;
+
 const endpoint = "holidaze/bookings";
-const url = proxyURL + "?endpoint=" + endpoint;
-//const url = `http://localhost:3000/auth` + "?endpoint=" + endpoint;
+//const url = proxyURL + "?endpoint=" + endpoint;
+const url = base + "?endpoint=" + endpoint;
+console.log("this is the url", url);
 export default async function bookVenueAction(state: any, formData: FormData) {
   const temp = formDataToObject(formData);
   const dateFrom = new Date(String(temp.dateFrom));
@@ -27,6 +30,8 @@ export default async function bookVenueAction(state: any, formData: FormData) {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
+
       body: JSON.stringify(requestBody),
     });
     const data = await response.json();
