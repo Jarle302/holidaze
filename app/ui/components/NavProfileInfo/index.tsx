d8b785df16d1d27aad1fee5894467621e48df893
Jarle302 <jarlehtollaksen2@live.no>
2024-06-01 14:25:01 +0200

    fix nav

--- app/ui/components/NavProfileInfo/index.tsx
+++ app/ui/components/NavProfileInfo/index.tsx
@@ -2,19 +2,21 @@
 
 import Link from "next/link";
 import { userInfoContext } from "../UseInfoProvider";
+import { usePathname } from "next/navigation";
 import { useContext } from "react";
 import { LogOutButton } from "../Header/LogOutButton";
 import { IoHomeSharp, IoLogOutSharp } from "react-icons/io5";
 import { FaUser } from "react-icons/fa6";
 
 export const NavProfileInfo = () => {
+  const path = usePathname();
   const user = useContext(userInfoContext);
   console.log({ user });
   return (
     <nav>
       <ul className="flex gap-4">
         <li>
-          <Link className="flex" href="/">
+          <Link className={path === `/` ? "text--green flex" : "flex"} href="/">
             <IoHomeSharp />
             Home
           </Link>
@@ -22,20 +24,32 @@ export const NavProfileInfo = () => {
         {!user?.userInfo?.name ? (
           <>
             <li>
-              <Link href="/auth/register">Register</Link>
+              <Link
+                className={path === "/auth/register" ? "text--green" : ""}
+                href="/auth/register">
+                Register
+              </Link>
             </li>
             <li>
-              <Link href="/auth/login">Login</Link>
+              <Link
+                className={path === "/auth/login" ? "text--green" : ""}
+                href="/auth/login">
+                Login
+              </Link>
             </li>
           </>
         ) : (
           <>
             <li>
-              <Link className="flex" href={`/profile/${user?.userInfo?.name}`}>
+              <Link
+                className={
+                  path === `/profile/${user?.userInfo?.name}`
+                    ? "text--green flex"
+                    : "flex text-red-300 font-bold"
+                }
+                href={`/profile/${user?.userInfo?.name}`}>
                 <FaUser />
-                <span className="text-red-300 font-bold">
-                  {user?.userInfo?.name}
-                </span>
+                <span>{user?.userInfo?.name}</span>
               </Link>
             </li>
             <li className="flex">
