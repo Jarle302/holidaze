57a2adb570c925430f4cabcbd1c69878d93d7e8f
Jarle302 <jarlehtollaksen2@live.no>
2024-06-02 09:05:13 +0200

    add type to error usetoast

--- app/ui/utils/customHooks/useToast.ts
+++ app/ui/utils/customHooks/useToast.ts
@@ -5,7 +5,11 @@ export default function useToast(state: any, router?: any, path?: string) {
   useEffect(() => {
     console.log(state);
     if (state?.statusCode > 299) {
-      toast.error(state?.errors.map((error) => error.message).join(", "));
+      toast.error(
+        state?.errors
+          .map((error: { message: string }) => error.message)
+          .join(", ")
+      );
     }
     if (state?.data && Object.keys(state?.data).length > 0) {
       toast("Success!");
