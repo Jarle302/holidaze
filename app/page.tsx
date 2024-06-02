8330b13852814ddbd4419b714e150cb6a0d31b93
Jarle302 <jarlehtollaksen2@live.no>
2024-06-02 19:40:59 +0200

    add h2 to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ export default function Venues() {
   return (
     <main className="flex flex-wrap gap-4 justify-center sm:justify-start">
       <h1 className="text-slate-600   text-[50px] md:text-[100px] font-bold w-full">
-        Venues
+        Holidaze
       </h1>
+
+      <h2 className="text-4xl text-slate-700 font-bold">Venues</h2>
       <Search RenderComponent={VenueCard} />
       <VenueList />
     </main>
