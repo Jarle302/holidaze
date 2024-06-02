ef3431a15499732bcbe6c3d130755b068f2f899a
Jarle302 <jarlehtollaksen2@live.no>
2024-06-02 21:51:57 +0200

    make venuepage handle long words

--- app/venues/singleVenue/[id]/page.tsx
+++ app/venues/singleVenue/[id]/page.tsx
@@ -21,12 +21,18 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <main className="bg-zinc-200 md:p-[40px] p-[20px] flex flex-wrap gap-5">
-      <h1 className="text-[50px]  w-full text-slate-700 font-bold">{name}</h1>
-      <div className="flex flex-col max-w-[500px] gap-5">
+      <div className="w-full">
+        <h1 className="text-[50px] break-words sm:max-w-[300px]	 max-w-[300px] text-slate-700 font-bold">
+          {name}
+        </h1>
+      </div>
+      <div className="flex flex-col  max-w-[500px] gap-5">
         <div className="p-3 bg-white sm:w-[500px] sm:h-[500px] h-[320px] w-[280px] ">
           <Carousel fitToContainer={true} media={media} />
         </div>
-        <p>{description}</p>
+        <p className="break-words	sm:max-w-[300px]	 max-w-[300px]">
+          {description}
+        </p>
       </div>
       <div className="flex flex-col gap-5">
         <VenueBooker id={params.id} bookings={bookings} />
